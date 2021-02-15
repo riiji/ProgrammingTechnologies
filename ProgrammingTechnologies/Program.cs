aa1c439bb152c8220287cf0457bc660a6db1db17
Riijii <riijii333@gmail.com>
2021-02-15 12:55:30 +0300

    initial

---
+++ ProgrammingTechnologies/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProgrammingTechnologies
+{
+    class Program
+    {
+        /// <summary>
+        /// Some information about method
+        /// </summary>
+        /// <param name="args">Argument</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting project");
+        }
+    }
+}
